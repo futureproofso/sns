63906030488945350cc49e3fd34d1801bfdd18af
v-stickykeys <8445610+v-stickykeys@users.noreply.github.com>
2022-09-11 15:55:25 -0400

    feat: test secret name creation

---
+++ test/secretName.spec.ts
@@ -0,0 +1,36 @@
+import 'mocha'
+import { expect } from 'chai'
+
+import { Account, AccountExternal } from '../src/account'
+import { SecretName as SN } from '../src/secretName'
+import { createLutRecord, LutRecord } from '../src/lut'
+
+const emptyLutRecord: LutRecord = {
+    to: '',
+    from: '',
+    data: {
+        plaintext: '',
+        ciphertext: ''
+    }
+}
+
+
+describe('SecretName', () => {
+    it('starts with only a name', () => {
+        const secretName = new SN('stickykeys')
+        expect(secretName.name).to.equal('stickykeys')
+        expect(secretName.isExternal()).to.be.false
+        expect(secretName.secrets).to.deep.equal({})
+        expect(() => secretName.putSecret(emptyLutRecord)).to.throw()
+    })
+    describe('setController', () => {
+        it('throws if passed an external account', () => {
+            const acct = new Account()
+            const alex = new AccountExternal(acct.getPublicEncryptionKey(), acct.getPublicSigningKey().toString())
+            const sn = new SN('stickykeys')
+            expect(() => sn.setController(alex)).to.throw()
+        })
+    })
+})
+
+
--- test/utils.spec.ts
+++ test/utils.spec.ts
@@ -1,4 +1,3 @@
-
 import 'mocha'
 import { expect } from 'chai'
 import { chunk } from '../src/utils'
